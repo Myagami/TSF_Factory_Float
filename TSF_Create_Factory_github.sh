21859d39cb8e4f09fd01ede9ecdab8d1839b903e
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-07-27 00:53:29 +0900

    update update14/07/26 22:10:35

--- TSF_Create_Factory_github.sh
+++ TSF_Create_Factory_github.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-Today=`update for date +"%y/%m/%d %H:%I:%S"`;
-Today="Update for"$Today
-#git add * ;
+Today=`date +"%y/%m/%d %H:%I:%S"`;
+Today="Update for"$Today;
+git add * ;
 git commit -a -m $Today ;
 git push github master;
