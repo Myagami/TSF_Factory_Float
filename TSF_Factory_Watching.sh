cb6640028960c542c5bec260d66d714aaf5b1f2d
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-07-28 17:01:23 +0900

    14/07/28 17:05:23

--- TSF_Factory_Watching.sh
+++ TSF_Factory_Watching.sh
@@ -5,5 +5,5 @@ while inotifywait -e modify -e create -e delete -r ./ ; do
     Today='';
     Today=`date +"%y/%m/%d %H:%I:%S"`;
     git commit -m "$Today" ;
-    git push github master;
+    git push github master ;
 done
