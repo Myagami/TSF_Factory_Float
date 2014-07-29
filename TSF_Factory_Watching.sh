db3e98fd40e682c3d84417cf01bbd9668c43150c
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-07-29 11:15:28 +0900

    Update for 14/07/29 11:11:28

--- TSF_Factory_Watching.sh
+++ TSF_Factory_Watching.sh
@@ -4,6 +4,6 @@ while inotifywait -e modify -e create -e delete -r ./ ; do
     git add * ;
     Today='';
     Today=`date +"%y/%m/%d %H:%I:%S"`;
-    git commit -m "Update for $Today" ;
+    git commit -a -m "Update for $Today" ;
     git push github master ;
 done
