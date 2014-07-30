bf9fb34161f812cb31da21f8c1a10e5bb95008d1
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-07-30 13:17:50 +0900

    Update for 14/07/30 13:17:50

--- TSF_Factory_Watching.pl
+++ TSF_Factory_Watching.pl
@@ -7,3 +7,14 @@ use Linux::Inotify2 ;
 #このディレクトリ監視
 my $watch_dir =  './';
 my $inotify = Linux::Inotify2->new or die $!;
+
+$inotify->watch(
+    $watch_dir,
+    IN_CLOSE_WRITE | IN_MOVED_TO,
+    sub {
+        my $e    = shift;
+        my $name = $e->fullname;
+        print $name, "\n";
+
+    }
+);
