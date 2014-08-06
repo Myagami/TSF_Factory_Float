f002fdcb00c5533fb1522fafb25e22a7662bf5fc
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-08-06 18:14:53 +0900

    Update for 14/08/06 18:14:51

---
+++ TSF_Factory_Watching_Off.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+## 全体のモニター
+while inotifywait -e modify,create,delete -r ./ ; do
+    #追加ファイルを確認
+    git add * ;
+    #今日の日付
+    Today='';
+    Today=`date +"%y/%m/%d %T"`;
+    #commit
+    git commit -m "Update for $Today" -a  ;
+    #to github
+    git push github Devel ;
+done
+
