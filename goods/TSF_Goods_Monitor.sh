41316af8ac08b6f80b7722d71892e6070b1884ae
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-10-03 11:38:28 +0900

    Update for 14/10/03 11:38:28

--- goods/TSF_Goods_Monitor.sh
+++ goods/TSF_Goods_Monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ## 全体のモニター
 while inotifywait -e modify,create,delete -r ./ ; do
-    cp *.pak /home/hayate/simutrans/addons/pak.dev
+    cp `*.pak` /home/hayate/simutrans/addons/pak.dev
 done
 
