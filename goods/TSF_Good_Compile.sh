ac1115b2df95dae4e16da235ceec99831f515d17
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-10-04 17:34:31 +0900

    Update for 14/10/04 17:34:31

--- goods/TSF_Good_Compile.sh
+++ goods/TSF_Good_Compile.sh
@@ -3,4 +3,7 @@
 inotifywait -m -e modify,create,delete --format %w%f "TSF_Goods_Index.csv"|
 while read files;do
     ./TSF_Good_Compile.php
+    echo "---------------"
+    echo "Action complite"
+    echo "---------------"
 done
