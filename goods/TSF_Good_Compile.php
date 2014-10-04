0474f2a366de2e35d27185742a83b016b135ffb2
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-10-04 16:43:26 +0900

    Update for 14/10/04 16:43:25

--- goods/TSF_Good_Compile.php
+++ goods/TSF_Good_Compile.php
@@ -4,6 +4,6 @@ $csv = file("TSF_Goods_Index.csv") ;
 $Index = array_shift($csv) ;
 print_r($csv) ;
 foreach($csv as $dat){
-	print_r(explode(",",$dat)) ;
+	print_r(array_combine($Index,explode(",",$dat))) ;
 }
 ?>
