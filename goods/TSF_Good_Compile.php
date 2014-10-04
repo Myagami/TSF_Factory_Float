3ca951bee535ed10d8901944d321546ba67d95b5
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-10-04 16:48:52 +0900

    Update for 14/10/04 16:48:52

--- goods/TSF_Good_Compile.php
+++ goods/TSF_Good_Compile.php
@@ -1,9 +1,14 @@
 #!/usr/bin/php
 <?php
+   //ファイル読み込み
 $csv = file("TSF_Goods_Index.csv") ;
+
+//Index
 $Index = array_shift($csv) ;
 $Index = explode(",",$Index) ;
 //print_r($csv) ;
+
+//出力
 foreach($csv as $dat){
 	$dat = explode(",",$dat) ;
 	$C_dat = array_combine($Index,$dat) ;
