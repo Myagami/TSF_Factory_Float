f1275fcf8c86431e0f3988bb431cc414096fbe48
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-10-04 16:50:17 +0900

    Update for 14/10/04 16:50:17

--- goods/TSF_Good_Compile.php
+++ goods/TSF_Good_Compile.php
@@ -9,6 +9,7 @@ $Index = explode(",",$Index) ;
 $Index[9] = str_replace(array("\r\n","\n","\r"), '', $Index[9]) ;
 //Output
 foreach($csv as $dat){
+	//キーを渡す
 	$dat = explode(",",$dat) ;
 	$C_dat = array_combine($Index,$dat) ;
 	print_r($C_dat) ;
