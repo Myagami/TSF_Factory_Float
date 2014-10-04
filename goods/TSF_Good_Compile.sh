5631991d4f5e15becbfc5397185cbf04e2506134
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-10-04 17:37:50 +0900

    Update for 14/10/04 17:37:49

--- goods/TSF_Good_Compile.sh
+++ goods/TSF_Good_Compile.sh
@@ -3,7 +3,7 @@
 inotifywait -m -e modify,create,delete --format %w%f "TSF_Goods_Index.csv"|
 while read files;do
     ./TSF_Good_Compile.php
-    cp `*pak /home/hayate/simutrans/addons/pak.dev/`
+    cp `*.pak /home/hayate/simutrans/addons/pak.dev/`
     echo "---------------"
     echo "Action complite"
     echo "---------------"
